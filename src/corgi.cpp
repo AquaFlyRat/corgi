a46022611911935d9f5e62e83bcf4e3c926fe104
Barney Wilks <barneywilks1@gmail.com>
2019-07-21 18:51:40 +0100

    Added basic project setup

---
+++ src/corgi.cpp
@@ -0,0 +1,13 @@
+#include <SDL2/SDL.h>
+#include <fmt/core.h>
+
+int main()
+{
+  fmt::print("[libfmt Test] -> The answer is {0}.\n", 42);
+  
+  SDL_version version;
+  SDL_GetVersion(&version);
+  printf("[SDL Test]: Version -> %i.%i.%i\n", version.major, version.minor, version.patch);
+  
+  return 0;
+}
