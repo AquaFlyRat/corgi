283cd5185a2fef2be6441fdc4d53f8d0706bd8b1
HolyBlackCat <blckcat@inbox.ru>
2019-07-22 23:19:23 +0300

    Partially fixed MinGW builds.

--- src/corgi.cpp
+++ src/corgi.cpp
@@ -1,13 +1,13 @@
 #include <SDL.h>
 #include <fmt/core.h>
 
-int main()
+int main(int, char **)
 {
   fmt::print("[libfmt Test] -> The answer is {0}.\n", 42);
-  
+
   SDL_version version;
   SDL_GetVersion(&version);
   printf("[SDL Test]: Version -> %i.%i.%i\n", version.major, version.minor, version.patch);
-  
+
   return 0;
 }
